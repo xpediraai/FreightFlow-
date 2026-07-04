b0f5089f47b0740da44e264efcc0f8d1d247da58
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-07-05 03:26:54 +0530

    Transport Mode Config Master

---
+++ freightflow-backend/src/modules/Masters/TransportModeMasters/transportMode.routes.js
@@ -0,0 +1,31 @@
+/**
+ * @file transportMode.routes.js
+ * @description Express routes for Transport Mode APIs.
+ */
+const express = require("express");
+const router = express.Router();
+const transportModeController = require("./transportMode.controller");
+const { authenticateToken } = require("../../../middlewares/auth.middleware");
+
+// Protect all routes
+router.use(authenticateToken);
+
+// Create
+router.post("/", transportModeController.create);
+
+// List
+router.get("/", transportModeController.list);
+
+// Get by ID
+router.get("/:id", transportModeController.getById);
+
+// Update
+router.put("/:id", transportModeController.update);
+
+// Change Status
+router.patch("/:id/status", transportModeController.changeStatus);
+
+// Soft Delete
+router.delete("/:id", transportModeController.remove);
+
+module.exports = router;
