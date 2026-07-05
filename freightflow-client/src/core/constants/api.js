8c506c00b4c0c49eaafaf888d9327c6933eec428
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-07-06 02:29:06 +0530

    Dist folder upload

--- freightflow-client/src/core/constants/api.js
+++ freightflow-client/src/core/constants/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://13.62.128.158/api';
 
 export const API_ENDPOINTS = {
   AUTH: {
